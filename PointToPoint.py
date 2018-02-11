2ea9d59f25f0a0592f2f1c5ffa6700b725694114
Siroyan <1517053@ed.tus.ac.jp>
2018-02-12 02:36:58 +0900

    [add]We can get nice routes.
    ???????????????????????????

---
+++ PointToPoint.py
@@ -0,0 +1,16 @@
+import math
+import itertools
+
+A = (0,0)
+B = (3,4)
+
+x = B[0] - A[0]
+y = B[1] - A[1]
+
+base_list = []
+for i in range(0,x):
+    base_list.append(1)
+for i in range(0,y):
+    base_list.append(0)
+
+all_ways = list(set(list(itertools.permutations(base_list))))
