d01ed9a1bc1f8e617b62067ae16a7c3b526c5ab0
Siroyan <1517053@ed.tus.ac.jp>
2018-02-12 15:48:40 +0900

    [add]コメント
    コメント追記

--- PointToPoint.py
+++ PointToPoint.py
@@ -7,7 +7,9 @@ B = (3,4)
 x = B[0] - A[0]
 y = B[1] - A[1]
 
-#[x,---,x,y,---,y]の形式
+#[1,---,1,0,---,0]の形式
+#1 <-> x方向
+#0 <-> y方向
 base_list = []
 for i in range(0,x):
     base_list.append(1)
@@ -19,6 +21,7 @@ all_ways = list(set(list(itertools.permutations(base_list))))
 results_set = []
 #全ルートに対し、直進,直角の回数を調べ,results_setに格納
 for i in range(0,len(all_ways)):
+    #[直進回数,直角回数]
     way_result = [0,0]
     for j in range(0,len(all_ways[i])-1):
         if all_ways[i][j] == all_ways[i][j+1]:#straight
