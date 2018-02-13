f92479844e57a3ea791a6bf280b1830e00b9fff5
Siroyan <1517053@ed.tus.ac.jp>
2018-02-13 12:25:24 +0900

    [change]関数化
    座標を得るプログラムを関数化

--- PointToPoint.py
+++ PointToPoint.py
@@ -7,6 +7,7 @@ Ax = 0
 Ay = 0
 Bx = 3
 By = 4
+#最適なルートを選択
 def findRoute(Ax,Ay,Bx,By):
     A = (Ax,Ay)
     B = (Bx,By)
@@ -48,13 +49,14 @@ def findRoute(Ax,Ay,Bx,By):
     #ポイント最大のルートを表示
     for i in best_route_index:
         bestRoute.append(all_ways[i])
+#移動の1,0を座標に変換
+def getCoordinates():
+    for i in range (0,7):
+        if bestRoute[0][i] == 1:
+            Ax += 1
+        else:
+            Ay += 1
+        coordinates.append([Ax,Ay])
+    return coordinates
 
 findRoute(Ax,Ay,Bx,By)
-#移動の1,0を座標に変換
-for i in range (0,7):
-    if bestRoute[0][i] == 1:
-        Ax += 1
-    else:
-        Ay += 1
-    coordinates.append([Ax,Ay])
-print(coordinates)
